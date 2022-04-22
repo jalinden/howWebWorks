84e7d3461a8d5aa483d5458cfeee9aa200a55762
jalinden <97492113+jalinden@users.noreply.github.com>
2022-04-21 23:34:58 -0500

    Uploading completed files

---
+++ howWebWorksExercise.js
@@ -0,0 +1,35 @@
+//PART 1
+//1.
+'HTTP, the Hypertext Transfer Protocol, is the set' 
+'of standardized guidelines in place for web browsers and servers within'
+' a network to communicate to one another'
+
+//2.
+'A URL is a unique identifier referring to the location of specific content on the web'
+
+//3.
+'The DNS is the repository that maintains records of what IP addresses are correlated with what URLs'
+
+//4.
+'A query string is how one communicates very specific requests to a server'
+
+//5.
+'Two HTTP verbs are GET and POST. They both make requests but only POST has side effects'
+
+//6.
+'An HTTP request is how one asks for information contained in a server'
+
+//7.
+'An HTTP response is what one receives after they make a request'
+
+//8.
+'An HTTP header is metadata that is sent along with an HTTP request or response'
+'E.g. A request may come with a header designating that the response be in English'
+'A response may come with the date and time the server thinks it is'
+
+//9.
+'My system either already knows the IP associated with the URL or otherwise has to ask the DNS what IP is associated with the URL'
+'My system then requests that the server at that IP send the information from the webpage at that URL'
+'Finally my system receives that info from the server, my browser interprets the info, and then shows it to me'
+
+
